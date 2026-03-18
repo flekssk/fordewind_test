77d23ce1041425f928f53bca571193d909f93a11
Aleksei Osipov <a.osipov@sxope.com>
2026-03-18 04:16:41 +0400

    add missing providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
+    FKS\Serializer\SerializerProvider::class,
+    FKS\Search\SearchComponentProvider::class,
 ];
